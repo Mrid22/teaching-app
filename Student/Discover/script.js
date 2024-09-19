e4f3d58f4fd4389dde5b370a0ba8bd1e45db607c
carlos <carloschiu@biorhythm.hk>
2024-09-19 20:19:29 +0800

    stole stuff from home

---
+++ Student/Discover/script.js
@@ -0,0 +1,25 @@
+indicator=document.getElementById("indicator")
+home=document.getElementById("Home")
+discover=document.getElementById("Discover")
+account=document.getElementById("Account")
+
+indicator.style.left=home.offsetLeft+"px"
+indicator.style.width=home.offsetWidth+"px"
+
+discover.addEventListener("click",function(){
+    indicator.style.left=discover.offsetLeft+"px"
+    indicator.style.width=discover.offsetWidth+"px"
+})
+
+home.addEventListener("click",function(){
+    indicator.style.left=home.offsetLeft+"px"
+    indicator.style.width=home.offsetWidth+"px"
+})
+
+account.addEventListener("click",function(){
+    indicator.style.left=account.offsetLeft+"px"
+    indicator.style.width=account.offsetWidth+"px"
+    setTimeout(function(){
+        window.location.href="../Account/index.html"  
+    },500)
+})
