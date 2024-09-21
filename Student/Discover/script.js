4b4015987ed0e409f4d3eea062e7ca636e372dc7
Mridul Agarwal <mridul16381@chromium.org>
2024-09-21 14:53:12 +0800

    Added search functionality

--- Student/Discover/script.js
+++ Student/Discover/script.js
@@ -1,7 +1,18 @@
+search=document.getElementById("search-bar")
+courses=document.getElementById("courses")
+course_name=document.getElementsByClassName("course-name")
 indicator=document.getElementById("indicator")
 home=document.getElementById("Home")
 discover=document.getElementById("Discover")
 account=document.getElementById("Account")
+results=document.getElementById("results")
+resultsbox=document.getElementById("resultsbox")
+
+let courses_list=[]
+for (let i = 0; i < course_name.length; i++) {
+    courses_list.push(course_name[i].innerHTML)
+}
+
 
 indicator.style.left=discover.offsetLeft+"px"
 indicator.style.width=discover.offsetWidth+"px"
@@ -28,4 +39,19 @@ account.addEventListener("click",function(){
     setTimeout(function(){
         window.location.href="../Account/index.html"  
     },500)
+})
+
+search.addEventListener("input",function(){
+    resultsbox.innerHTML=""
+    if(search.value!="" && search.value!=" "){
+        results=courses_list.filter(function(course){
+            return course.toLowerCase().includes(search.value.toLowerCase())
+         })
+         for (let i = 0; i < results.length; i++) {
+           p = document.createElement("p")
+            resultsbox.append(p)
+            p.innerHTML=results[i]
+            
+         }
+    }
 })
