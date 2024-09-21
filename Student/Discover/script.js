9fb3df73b1a811390982234dab003dd1fc540e96
carlos <carloschiu@biorhythm.hk>
2024-09-21 16:12:56 +0800

    new stuff

---
+++ Student/Discover/script.js
@@ -0,0 +1,31 @@
+indicator=document.getElementById("indicator")
+home=document.getElementById("Home")
+discover=document.getElementById("Discover")
+account=document.getElementById("Account")
+
+indicator.style.left=discover.offsetLeft+"px"
+indicator.style.width=discover.offsetWidth+"px"
+
+discover.addEventListener("click",function(){
+    indicator.style.left=discover.offsetLeft+"px"
+    indicator.style.width=discover.offsetWidth+"px"
+    setTimeout(function(){
+        window.location.href="index.html"  
+    },500)
+})
+
+home.addEventListener("click",function(){
+    indicator.style.left=home.offsetLeft+"px"
+    indicator.style.width=home.offsetWidth+"px"
+    setTimeout(function(){
+        window.location.href="../Home/index.html"  
+    },500)
+})
+
+account.addEventListener("click",function(){
+    indicator.style.left=account.offsetLeft+"px"
+    indicator.style.width=account.offsetWidth+"px"
+    setTimeout(function(){
+        window.location.href="../Account/index.html"  
+    },500)
+})
